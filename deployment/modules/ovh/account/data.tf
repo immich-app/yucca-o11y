973e2ced23c1514463e7a8df13476bb0b92a4a60
izzy <me@insrt.uk>
2025-10-29 16:21:55 +0000

    feat: add DNS zone & records

---
+++ deployment/modules/ovh/account/data.tf
@@ -0,0 +1,12 @@
+data "ovh_me" "myaccount" {}
+
+data "ovh_order_cart" "mycart" {
+  ovh_subsidiary = data.ovh_me.myaccount.ovh_subsidiary
+}
+
+data "ovh_order_cart_product_plan" "zone" {
+  cart_id        = data.ovh_order_cart.mycart.id
+  price_capacity = "renew"
+  product        = "dns"
+  plan_code      = "zone"
+}
