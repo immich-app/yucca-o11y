2a13b295e5c08266c32acbcd8207f2424a29b8b9
Zack <zack@futo.org>
2026-02-24 15:59:11 +0000

    chore: wip

--- deployment/modules/kubernetes/helm/providers.tf
+++ deployment/modules/kubernetes/helm/providers.tf
@@ -9,3 +9,13 @@ provider "helm" {
     cluster_ca_certificate = base64decode(each.value.ca_certificate)
   }
 }
+
+provider "kubernetes" {
+  alias    = "cluster"
+  for_each = var.clusters
+
+  host                   = each.value.endpoint
+  client_certificate     = base64decode(each.value.client_certificate)
+  client_key             = base64decode(each.value.client_key)
+  cluster_ca_certificate = base64decode(each.value.ca_certificate)
+}
